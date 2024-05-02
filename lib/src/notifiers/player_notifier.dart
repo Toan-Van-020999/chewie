8fb7a211a3324cb89c5319830cdf68b92c3fe409
Toan-Van-020999 <toanvan020999@gmail.com>
2024-05-02 09:35:17 +0700

    Update player_notifier.dart

--- lib/src/notifiers/player_notifier.dart
+++ lib/src/notifiers/player_notifier.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
 ///
 /// The new State-Manager for Chewie!
 /// Has to be an instance of Singleton to survive
@@ -10,9 +11,40 @@ class PlayerNotifier extends ChangeNotifier {
     bool hideStuff,
   ) : _hideStuff = hideStuff;
 
+  Timer? _zoomDisplayTimer;
+  bool _isZooming = false;
+
+  bool get isZooming => _isZooming;
+
+  set isZooming(bool value) {
+    _isZooming = value;
+    notifyListeners();
+  }
+
+  void startZoomDisplayTimer() {
+    _zoomDisplayTimer?.cancel();
+    _zoomDisplayTimer = Timer(const Duration(seconds: 2), () {
+      isZooming = false;
+    });
+  }
+
+  void cancelZoomDisplayTimer() {
+    _zoomDisplayTimer?.cancel();
+  }
+
+
   bool _hideStuff;
 
   bool get hideStuff => _hideStuff;
+
+  double _zoomValue = 1.0;
+
+  set zoomValue(double value) {
+    _zoomValue = value;
+    notifyListeners();
+  }
+
+  double get zoomValue => _zoomValue;
 
   set hideStuff(bool value) {
     _hideStuff = value;
